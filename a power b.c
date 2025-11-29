7973e11486eb7cb1d665cf723224472f72858539
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-29 13:52:09 +0530

    Add files via upload

---
+++ a power b.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int a_b(int,int);
+int main(){
+    int a,b,power;
+    printf("Enter two numbers");
+    scanf("%d %d",&a,&b);
+    power=a_b(a,b);
+    printf("Power of %d to the %d is %d", a, b, power);
+    return 0;
+}
+int a_b(int a,int b){
+    if(b==0){
+        return 1;
+    }
+    else{
+        return a*a_b(a,b-1);
+    }
+}

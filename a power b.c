2c7de66c70240b898a1430b48da00ffec8ac25e7
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-29 13:53:40 +0530

    Refactor input prompts for power calculation

--- a power b.c
+++ a power b.c
@@ -2,8 +2,10 @@
 int a_b(int,int);
 int main(){
     int a,b,power;
-    printf("Enter two numbers");
-    scanf("%d %d",&a,&b);
+    printf("Enter A:");
+    scanf("%d",&a);
+    printf("Enter B:");
+    scanf("%d",&b);
     power=a_b(a,b);
     printf("Power of %d to the %d is %d", a, b, power);
     return 0;
@@ -15,4 +17,5 @@ int a_b(int a,int b){
     else{
         return a*a_b(a,b-1);
     }
+
 }
